acef9fd10662058b89d19c31c5e0f55ca5d44d22
zeeshanjunaid <zeeshanjunaid2222@gmail.com>
2020-08-02 04:54:08 +0500

    header updated

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,16 @@ import {
   HeaderMenuRight,
 } from "../styles/headerStyle";
 import uuid from "react-uuid";
-import { MenuIcon, FlyButton, Chevron, Pen } from "./icons";
+import {
+  MenuIcon,
+  FlyButton,
+  Chevron,
+  Pen,
+  Toolbar,
+  Hand,
+  Location,
+  Play,
+} from "./icons";
 const menuLeftData = [
   {
     id: 1,
@@ -26,12 +35,45 @@ const menuLeftData = [
     hasDropDown: true,
     dropDownContent: "content",
   },
+  {
+    id: 4,
+    icon: <Toolbar />,
+    hasDropDown: false,
+    dropDownContent: "",
+  },
+  {
+    id: 5,
+    icon: <Location />,
+    hasDropDown: false,
+    dropDownContent: "",
+  },
+  {
+    id: 6,
+    icon: <Hand />,
+    hasDropDown: false,
+    dropDownContent: "",
+  },
+];
+
+const menuRightData = [
+  {
+    id: 11,
+    icon: <Play />,
+    hasDropDown: false,
+    dropDownContent: "",
+  },
+  {
+    id: 12,
+    icon: "22%",
+    hasDropDown: true,
+    dropDownContent: "content",
+  },
 ];
 const Header = () => {
-  const [active, setActive] = useState(false);
+  const [active, setActive] = useState("");
   const handleClick = (item) => {
     if (active === item.id) {
-      setActive(false);
+      setActive("");
     } else {
       setActive(item.id);
     }
@@ -58,13 +100,48 @@ const Header = () => {
               </span>
             )}
             {active === item.id && item.dropDownContent !== "" && (
-              <div class="dropdown">{item.dropDownContent}</div>
+              <div className="dropdown">{item.dropDownContent}</div>
             )}
           </li>
         ))}
       </HeaderMenuLeft>
-      <HeaderMenuCenter>header center</HeaderMenuCenter>
-      <HeaderMenuRight>header right</HeaderMenuRight>
+      <HeaderMenuCenter>
+        <a href="#">Drafts</a>
+        <span className="dash-mark">/</span>
+        <a href="#">
+          Untitled
+          <span>
+            <Chevron />
+          </span>
+        </a>
+      </HeaderMenuCenter>
+      <HeaderMenuRight>
+        <a className="dpIcon" href="#">Z</a>
+        <button>Share</button>
+        {menuRightData.map((item) => (
+          <li
+            key={uuid()}
+            className={
+              item.id === active &&
+              "activeClass " +
+                (active === item.id &&
+                  item.dropDownContent !== "" &&
+                  "hasDropDown")
+            }
+            onClick={() => handleClick(item)}
+          >
+            {item.icon}
+            {item.hasDropDown && (
+              <span>
+                <Chevron />
+              </span>
+            )}
+            {active === item.id && item.dropDownContent !== "" && (
+              <div className="dropdown">{item.dropDownContent}</div>
+            )}
+          </li>
+        ))}
+      </HeaderMenuRight>
     </HeaderArea>
   );
 };
--- src/styles/headerStyle.js
+++ src/styles/headerStyle.js
@@ -8,18 +8,17 @@ export const HeaderArea = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  ul {
-    list-style: none;
-    display: flex;
-    li {
-      padding: 1rem;
-      display: flex;
-      align-items: center;
-    }
-  }
 `;
-export const HeaderMenuLeft = styled.ul`
+export const HeaderMenuLeft = styled.div`
+  list-style: none;
+  display: flex;
+  flex-grow: 1;
+  flex-basis: 0;
   li {
+    height: 4rem;
+    padding: 1rem;
+    display: flex;
+    align-items: center;
     transition: all 0.3s;
     position: relative;
     overflow: visible;
@@ -64,5 +63,119 @@ export const HeaderMenuLeft = styled.ul`
   }
 `;
 
-export const HeaderMenuCenter = styled.ul``;
-export const HeaderMenuRight = styled.ul``;
+export const HeaderMenuCenter = styled.div`
+  flex-grow: 1;
+  flex-basis: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.4rem;
+  span.dash-mark {
+    padding: 0 0.5rem;
+  }
+  a {
+    font-size: 1.4rem;
+    transition: all 0.3s;
+    &:hover {
+      cursor: default;
+    }
+    &:first-child {
+      color: #aaaaaa;
+      &:hover {
+        color: ${(props) => props.theme.whiteColor};
+      }
+    }
+    &:last-child {
+      color: ${(props) => props.theme.whiteColor} !important;
+      svg {
+        transition: all 0.3s;
+        margin-left: 0.5rem;
+      }
+      &:hover {
+        span svg {
+          transform: translateY(0.3rem);
+        }
+      }
+    }
+  }
+`;
+export const HeaderMenuRight = styled.div`
+  list-style: none;
+  display: flex;
+  flex-grow: 1;
+  flex-basis: 0;
+  justify-content: flex-end;
+  align-items: center;
+  li {
+    height: 4rem;
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    transition: all 0.3s;
+    position: relative;
+    overflow: visible;
+
+    span {
+      margin-left: 0.75rem;
+      transition: all 0.3s;
+    }
+    &:hover {
+      background-color: ${(props) => props.theme.blackColor};
+      span {
+        transform: translateY(0.3rem);
+      }
+    }
+    &.activeClass {
+      background-color: ${(props) => props.theme.blueColor};
+    }
+    &.hasDropDown {
+      &::after {
+        content: "";
+        width: 1.2rem;
+        height: 1.2rem;
+        background: ${(props) => props.theme.blackColor};
+        position: absolute;
+        bottom: -1.5rem;
+        right: 1rem;
+        transform: rotate(45deg);
+        z-index: 10;
+      }
+    }
+
+    .dropdown {
+      width: 10rem;
+      height: 4rem;
+      background: ${(props) => props.theme.blackColor};
+      position: absolute;
+      top: 115%;
+      left: 0;
+      z-index: 11;
+      padding: 1rem;
+    }
+  }
+  button {
+    background: ${(props) => props.theme.blueColor};
+    color: ${(props) => props.theme.whiteColor};
+    border: 0px;
+    font-size: 1.1rem;
+    font-weight: bold;
+    width: 6rem;
+    height: 3rem;
+    border-radius: 0.8rem;
+    text-align: center;
+    margin-right: 1rem;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+  .dpIcon {
+    color: ${(props) => props.theme.whiteColor};
+    background: #fbc701;
+    padding: .8rem 1.1rem;
+    font-size: 1.2rem;
+    margin-right: 1rem;
+    border-radius: 150%;
+    text-align: center;
+  }
+`;
